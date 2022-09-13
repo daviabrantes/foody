c60d1ffd29e23a5d4adc9cb118d2745da2a32b5d
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-09-13 14:20:19 -0300

    Data Classes

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.foody
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com/recipes/complexSearch"
+        const val API_KEY = "1dba483d31154e5aa8e999a6bf73cef2"
+    }
+}
