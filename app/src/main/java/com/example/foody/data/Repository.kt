6599e710989f671efc4bdd1bb5e78c7f661e6b0a
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-09-18 15:16:08 -0300

    Repository and DI

---
+++ app/src/main/java/com/example/foody/data/Repository.kt
@@ -0,0 +1,13 @@
+package com.example.foody.data
+
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
+import javax.inject.Scope
+
+@ActivityRetainedScoped
+class Repository @Inject constructor(
+    remoteDataSource: RemoteDataSource
+) {
+
+    val remote = remoteDataSource
+}
